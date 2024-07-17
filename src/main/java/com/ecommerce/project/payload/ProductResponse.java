406329b56a4cba88495f420a5bc7e419d462d70d
MohitSainiDev <mht4943@gmail.com>
2024-07-17 13:30:55 +0530

    Added update product Image and pagination as well as sorting

--- src/main/java/com/ecommerce/project/payload/ProductResponse.java
+++ src/main/java/com/ecommerce/project/payload/ProductResponse.java
@@ -14,4 +14,9 @@ import lombok.NoArgsConstructor;
 @Component
 public class ProductResponse {
 	private List<ProductDTO> content;
+	private Integer pageNumber;
+	private Integer pageSize;
+	private Long totalElements;
+	private Integer totalPages;
+	private boolean lastPage;
 }
--- src/main/java/com/ecommerce/project/service/ProductService.java
+++ src/main/java/com/ecommerce/project/service/ProductService.java
@@ -1,21 +1,29 @@
 package com.ecommerce.project.service;
 
-import com.ecommerce.project.model.Product;
+import java.io.IOException;
+
+import org.springframework.web.multipart.MultipartFile;
+
 import com.ecommerce.project.payload.ProductDTO;
 import com.ecommerce.project.payload.ProductResponse;
 
 public interface ProductService {
 
-	ProductDTO addProduct(Long categoryId, Product product);
+	ProductDTO addProduct(Long categoryId, ProductDTO productDTO);
 
-	ProductResponse getAllProducts();
+	ProductResponse getAllProducts(Integer pageNumber, Integer pageSize, String sortBy, String sortOrder);
 
-	ProductResponse searchByCategory(Long categoryId);
+	ProductResponse searchByCategory(Long categoryId, Integer pageNumber, Integer pageSize, String sortBy,
+			String sortOrder);
 
-	ProductResponse searchProductByKeyWord(String keyWord);
+	ProductResponse searchProductByKeyWord(String keyWord, Integer pageNumber, Integer pageSize, String sortBy,
+			String sortOrder);
 
-	ProductDTO updateProduct(Long productId, Product product);
+	ProductDTO updateProduct(Long productId, ProductDTO productDTO);
 
 	ProductDTO deleteProduct(Long productId);
+
+	ProductDTO updateProductImage(Long productId, MultipartFile image) throws IOException;
+
 
 }
